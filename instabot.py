4113cf735861eafc6855c9530590fb23ab6054ee
Er. Munisha Bhasin <Er. Munisha Bhasin>
2017-07-12 00:37:41 +0530

    to get recent media liked by user,list of likes

--- instabot.py
+++ instabot.py
@@ -1,3 +1,4 @@
+#to accessed API through HTTP verbs we have use request library
 import requests
 #urllib helps us in fetching data across the web.
 import urllib
@@ -19,6 +20,7 @@ def self_info():
   user_info = requests.get(request_url).json()
   if user_info['meta']['code'] == 200:
       if len(user_info['data']):
+          # to get the username, number of follower, posts and people we follow from
           print 'Username is: %s' % (user_info['data']['username'])
           print 'No. of followers are: %s' % (user_info['data']['counts']['followed_by'])
           print 'No. of people you are following: %s' % (user_info['data']['counts']['follows'])
@@ -125,6 +127,7 @@ def get_post_id(insta_username):
     else:
         print 'Status code other than 200 received!'
         exit()
+
 #Function declaration to like the recent post of a user
 def like_a_post(insta_username):
   media_id = get_post_id(insta_username)
@@ -160,7 +163,6 @@ def post_a_comment(insta_username):
   else:
       print "Unable to add comment. Try again!"
 
-
 #function declaration to get the list of comments on a post .it accepts the user's username
 def get_comment_list(insta_username):
     #use of function get_post_id to get madia_id
@@ -178,7 +180,76 @@ def get_comment_list(insta_username):
     else:
         print" error!status code other then 200 received"
 
+#function declaration to get a list of people who likes the recent post
+def get_like_list(insta_username):
+    #use of function get_post_id to get madia_id
+    media_id=get_post_id(insta_username)
+    request_url=(BASE_URL+"media/%s/likes?access_token=%s")%((media_id,APP_ACCESS_TOKEN))
+    print"get request url:%s"%(request_url)
+    get_list=requests.get(request_url).json()
+    if get_list['meta']['code'] == 200:
+        i=0
+        while(i<len(get_list['data'])):
+            print "liked by %s"%(get_list['data'][i]['username'])
+            i=i+1
+        #printing of meesage on getting all user that liked
+        print " yipeeee! username who liked post are fetched successfully."
+    else:
+        print" error!status code other then 200 received"
 
+#function declaration to get the recent media liked  by the user
+def recent_media_liked():
+    request_url=(BASE_URL+"users/self/media/liked?access_token=%s")%(APP_ACCESS_TOKEN)
+    print"get request url:%s" % (request_url)
+    media_liked=requests.get(request_url).json()
+    if media_liked['meta']['code']==200:
+        if len(media_liked['data']):
+            image_name = media_liked['data'][0]['id'] + '.jpeg'
+            image_url = media_liked['data'][0]['images']['standard_resolution']['url']
+            urllib.urlretrieve(image_url, image_name)
+            print 'Your image has been downloaded!'
+            return media_liked['data'][0]['id']
+        else:
+            print "There is no recent post!"
+    else:
+        print "Status code other than 200 received!"
+    return None
+
+#function declaration to delete negative comments from a given user's post. It should accept the users' username as the input parameters.
+def delete_negative_comment(insta_username):
+    media_id = get_post_id(insta_username)
+    request_url = (BASE_URL + 'media/%s/comments/?access_token=%s') % (media_id, APP_ACCESS_TOKEN)
+    print 'GET request url : %s' % (request_url)
+    comment_info = requests.get(request_url).json()
+
+    if comment_info['meta']['code'] == 200:
+        if len(comment_info['data']):
+            #code to delete a comment
+            for x in range(0, len(comment_info['data'])):
+                comment_id = comment_info['data'][x]['id']
+                comment_text = comment_info['data'][x]['text']
+                #analyse the intent using the TextBlob library and look for comments with negative intent.
+                blob = TextBlob(comment_text, analyzer=NaiveBayesAnalyzer())
+                if (blob.sentiment.p_neg > blob.sentiment.p_pos):
+                    print 'Negative comment : %s' % (comment_text)
+                    delete_url = (BASE_URL + 'media/%s/comments/%s/?access_token=%s') % (
+                    media_id, comment_id, APP_ACCESS_TOKEN)
+                    print 'DELETE request url : %s' % (delete_url)
+                    #make a delete call to delete the comment with negative intent
+                    delete_info = requests.delete(delete_url).json()
+
+                    #printing of meaningfull msg on the successfull deletion of negative comment
+                    if delete_info['meta']['code'] == 200:
+                        print 'Comment successfully deleted!\n'
+                    #error in deleting comment
+                    else:
+                        print 'Unable to delete comment!'
+                else:
+                    print 'Positive comment : %s\n' % (comment_text)
+        else:
+            print 'There are no existing comments on the post!'
+    else:
+        print 'Status code other than 200 received!'
 
 
 
@@ -186,8 +257,10 @@ def get_comment_list(insta_username):
 def start_bot():
     while True:
         print '\n'
-        print 'Hey there! Welcome to instaBot!(*_*)'
+        print 'Hey there! Welcome to instaBot!(*_*).This bot will be capable of getting the information of users, liking their posts, deleting and making comments and much more'
         print 'Here are your menu options:'
+
+
         #menu to choose which task is to be performed
         print "a.Get your own details\n"
         print "b.Get details of a user by username\n"
@@ -197,8 +270,8 @@ def start_bot():
         print "f.Like the recent post of a user\n"
         print "g.Get a list of comments on the recent post of a user\n"
         print "h.Make a comment on the recent post of a user\n"
-        #print "i.Delete negative comments from the recent post of a user\n"
-        #print "j.to get the recent media liked by the user."
+        print "i.Delete negative comments from the recent post of a user\n"
+        print "j.to get the recent media liked by the user.\n"
         print "k.Exit"
 
         choice=raw_input("Enter you choice: ")
@@ -212,9 +285,9 @@ def start_bot():
         elif choice=="d":
            insta_username = raw_input("Enter the username of the user: ")
            get_user_post(insta_username)
-        #elif choice=="e":
-            #insta_username = raw_input("Enter the username of the user: ")
-            #get_like_list(insta_username)
+        elif choice=="e":
+            insta_username = raw_input("Enter the username of the user: ")
+            get_like_list(insta_username)
         elif choice=="f":
            insta_username = raw_input("Enter the username of the user: ")
            like_a_post(insta_username)
@@ -224,11 +297,11 @@ def start_bot():
         elif choice=="h":
             insta_username = raw_input("Enter the username of the user: ")
             post_a_comment(insta_username)
-        #elif choice=="i":
-            #insta_username = raw_input("Enter the username of the user: ")
-            #delete_negative_comment(insta_username)
-        #elif choice=="j":
-        #    recent_media_liked()
+        elif choice=="i":
+            insta_username = raw_input("Enter the username of the user: ")
+            delete_negative_comment(insta_username)
+        elif choice=="j":
+            recent_media_liked()
         elif choice=="k":
             exit()
         else:
